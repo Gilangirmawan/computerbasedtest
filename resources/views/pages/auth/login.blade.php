e4454a84dd2159a13d8014307d95179e4b5e3ed4
Gilangirmawan <98594634+Gilangirmawan@users.noreply.github.com>
2025-10-17 20:52:30 +0700

    Fix Complete Project

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -86,7 +86,7 @@
                             <div class="col-lg-12">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Selamat Datang di Sistem CBT SMK Muhamadiyah 01 Sirampog</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Selamat Datang di Sistem CBT</h1>
                                     </div>
                                     <form class="user" action="{{ route('login.authenticate') }}" method="post">
                                         @csrf
