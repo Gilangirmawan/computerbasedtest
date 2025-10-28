457736036cd49760833d8a215ae8c7e8176a4ce6
Gilangirmawan <98594634+Gilangirmawan@users.noreply.github.com>
2025-10-28 10:52:29 +0700

    fix bug

--- resources/views/pages/Jurusan/index.blade.php
+++ resources/views/pages/Jurusan/index.blade.php
@@ -13,7 +13,7 @@
         <h5><i class="fas fa-info-circle"></i> Catatan Admin</h5>
         <ul>
             <li>Data jurusan yang ada sekarang tidak bisa dihapus ataupun diedit jika data tersebut sedang digunakan pada bagian kelas.</li>
-            <li>Solusinya adalah mengahapus data kelas yang berkaitan dngan jurusan terlenih dahulu kemudian baru edit atau hapus data jurusan.</li>
+            <li>Solusinya adalah mengahapus data kelas yang berkaitan dngan jurusan terlebih dahulu kemudian baru edit atau hapus data jurusan.</li>
         </ul>
     </div>
 
